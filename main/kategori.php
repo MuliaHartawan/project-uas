fe3538c373c03c56987f92829a2dd38e8f289085
MuliaHartawan <mulia.hartawan011@gmail.com>
2021-05-04 13:20:14 +0700

    add product

--- main/kategori.php
+++ main/kategori.php
@@ -2,92 +2,12 @@
 session_start();
 include 'dbconnect.php';
 
-$idproduk = $_GET['idproduk'];
-
-if(isset($_POST['addprod'])){
-	if(!isset($_SESSION['log']))
-		{	
-			header('location:login.php');
-		} else {
-				$ui = $_SESSION['id'];
-				$cek = mysqli_query($conn,"select * from cart where userid='$ui' and status='Cart'");
-				$liat = mysqli_num_rows($cek);
-				$f = mysqli_fetch_array($cek);
-				$orid = $f['orderid'];
-				
-				//kalo ternyata udeh ada order id nya
-				if($liat>0){
-							
-							//cek barang serupa
-							$cekbrg = mysqli_query($conn,"select * from detailorder where idproduk='$idproduk' and orderid='$orid'");
-							$liatlg = mysqli_num_rows($cekbrg);
-							$brpbanyak = mysqli_fetch_array($cekbrg);
-							$jmlh = $brpbanyak['qty'];
-							
-							//kalo ternyata barangnya ud ada
-							if($liatlg>0){
-								$i=1;
-								$baru = $jmlh + $i;
-								
-								$updateaja = mysqli_query($conn,"update detailorder set qty='$baru' where orderid='$orid' and idproduk='$idproduk'");
-								
-								if($updateaja){
-									echo " <div class='alert alert-success'>
-								Barang sudah pernah dimasukkan ke keranjang, jumlah akan ditambahkan
-							  </div>
-							  <meta http-equiv='refresh' content='1; url= product.php?idproduk=".$idproduk."'/>";
-								} else {
-									echo "<div class='alert alert-warning'>
-								Gagal menambahkan ke keranjang
-							  </div>
-							  <meta http-equiv='refresh' content='1; url= product.php?idproduk=".$idproduk."'/>";
-								}
-								
-							} else {
-							
-							$tambahdata = mysqli_query($conn,"insert into detailorder (orderid,idproduk,qty) values('$orid','$idproduk','1')");
-							if ($tambahdata){
-							echo " <div class='alert alert-success'>
-								Berhasil menambahkan ke keranjang
-							  </div>
-							<meta http-equiv='refresh' content='1; url= product.php?idproduk=".$idproduk."'/>  ";
-							} else { echo "<div class='alert alert-warning'>
-								Gagal menambahkan ke keranjang
-							  </div>
-							 <meta http-equiv='refresh' content='1; url= product.php?idproduk=".$idproduk."'/> ";
-							}
-							};
-				} else {
-					
-					//kalo belom ada order id nya
-						$oi = crypt(rand(22,999),time());
-						
-						$bikincart = mysqli_query($conn,"insert into cart (orderid, userid) values('$oi','$ui')");
-						
-						if($bikincart){
-							$tambahuser = mysqli_query($conn,"insert into detailorder (orderid,idproduk,qty) values('$oi','$idproduk','1')");
-							if ($tambahuser){
-							echo " <div class='alert alert-success'>
-								Berhasil menambahkan ke keranjang
-							  </div>
-							<meta http-equiv='refresh' content='1; url= product.php?idproduk=".$idproduk."'/>  ";
-							} else { echo "<div class='alert alert-warning'>
-								Gagal menambahkan ke keranjang
-							  </div>
-							 <meta http-equiv='refresh' content='1; url= product.php?idproduk=".$idproduk."'/> ";
-							}
-						} else {
-							echo "gagal bikin cart";
-						}
-				}
-		}
-};
+$idk = $_GET['idkategori'];
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
-<title>Selikur Thrift - Produk</title>
+<title>Selikur Thrift - Kategori</title>
 <!-- for-mobile-apps -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -228,7 +148,7 @@ if(isset($_POST['addprod'])){
 										</ul>
 									</li>
 									<li><a href="cart.php">Keranjang Saya</a></li>
-									<li><a href="konfirmasi.php">Daftar Order</a></li>
+									<li><a href="daftarorder.php">Daftar Order</a></li>
 								</ul>
 							</div>
 							</nav>
@@ -241,58 +161,85 @@ if(isset($_POST['addprod'])){
 		<div class="container">
 			<ol class="breadcrumb breadcrumb1 animated wow slideInLeft" data-wow-delay=".5s">
 				<li><a href="index.php"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>Home</a></li>
-				<li class="active"><?php 
-				$p = mysqli_fetch_array(mysqli_query($conn,"Select * from produk where idproduk='$idproduk'"));
-				echo $p['namaproduk'];
-				?></li>
+				<li class="active">Kategori</li>
 			</ol>
 		</div>
 	</div>
 <!-- //breadcrumbs -->
+<!--- beverages --->
 	<div class="products">
 		<div class="container">
-			<div class="agileinfo_single">
-				
-				<div class="col-md-4 agileinfo_single_left">
-					<img id="example" src="<?php echo $p['gambar']?>" alt=" " class="img-responsive">
-				</div>
-				<div class="col-md-8 agileinfo_single_right">
-				<h2><?php echo $p['namaproduk'] ?></h2>
-					<div class="rating1">
-						<span class="starRating">
-							<?php
-								$bintang = '<i class="fa fa-star blue-star" aria-hidden="true"></i>';
-								$rate = $p['rate'];
-								
-								for($n=1;$n<=$rate;$n++){
-								echo '<i class="fa fa-star blue-star" aria-hidden="true"></i>';
-								};
-								?>
-						</span>
-					</div>
-					<div class="w3agile_description">
-						<h4>Deskripsi :</h4>
-						<p><?php echo $p['deskripsi'] ?></p>
-					</div>
-					<div class="snipcart-item block">
-						<div class="snipcart-thumb agileinfo_single_right_snipcart">
-							<h4 class="m-sing">Rp<?php echo number_format($p['hargaafter']) ?> <span>Rp<?php echo number_format($p['hargabefore']) ?></span></h4>
-						</div>
-						<div class="snipcart-details agileinfo_single_right_details">
-							<form action="#" method="post">
-								<fieldset>
-									<input type="hidden" name="idprod" value="<?php echo $idproduk ?>">
-									<input type="submit" name="addprod" value="Add to cart" class="button">
-								</fieldset>
-							</form>
-						</div>
-					</div>
-				</div>
-				<div class="clearfix"> </div>
+			<div class="col-md-4 products-left">
+				<div class="categories">
+					<h2>Categories</h2>
+					<ul class="cate">
+						
+						
+						<?php 
+														$kat=mysqli_query($conn,"SELECT * from kategori order by idkategori ASC");
+														while($p=mysqli_fetch_array($kat)){
+
+															?>
+														<li><a href="kategori.php?idkategori=<?php echo $p['idkategori'] ?>"><i class="fa fa-arrow-right" aria-hidden="true"></i><?php echo $p['namakategori'] ?></a></li>
+																				
+														<?php
+																	}
+														?>
+							
+					</ul>
+				</div>																																												
 			</div>
+			<div class="col-md-8 products-right">
+				<div class="agile_top_brands_grids">
+				
+				
+				<?php 
+					$brgs=mysqli_query($conn,"SELECT * from produk where idkategori='$idk' order by idproduk ASC");
+					$x = mysqli_num_rows($brgs);
+					
+					if($x>0){
+					while($p=mysqli_fetch_array($brgs)){
+					?>
+						
+						<div class="col-md-4 top_brand_left">
+						<div class="hover14 column">
+							<div class="agile_top_brand_left_grid">
+								<div class="agile_top_brand_left_grid_pos">
+									<img src="images/offer.png" alt=" " class="img-responsive" />
+								</div>
+								<div class="agile_top_brand_left_grid1">
+									<figure>
+										<div class="snipcart-item block">
+											<div class="snipcart-thumb">
+												<a href="product.php?idproduk=<?php echo $p['idproduk'] ?>"><img src="<?php echo $p['gambar']?>" width="200px" height="200px"></a>		
+												<p><?php echo $p['namaproduk'] ?></p>
+												<h4>Rp<?php echo number_format($p['hargaafter']) ?> <span>Rp<?php echo number_format($p['hargabefore']) ?></span></h4>
+											</div>
+											<div class="snipcart-details top_brand_home_details">
+												<fieldset>
+													<a href="product.php?idproduk=<?php echo $p['idproduk'] ?>"><input type="submit" class="button" value="Lihat Produk" /></a>
+												</fieldset>
+											</div>
+										</div>
+									</figure>
+								</div>
+							</div>
+						</div>
+					</div>
+						<?php
+							}
+					} else {
+						echo "Data tidak ditemukan";
+					}
+						?>
+					
+						<div class="clearfix"> </div>
+				</div>
+			</div>
+			<div class="clearfix"> </div>
 		</div>
 	</div>
-
+<!--- beverages --->
 <!-- //footer -->
 <div class="footer">
 		<div class="container">
