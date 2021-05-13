2f12eeb3e2886bbdcf2fa6901ddbe4bcab9acb4b
MuliaHartawan <mulia.hartawan011@gmail.com>
2021-05-13 17:22:59 +0700

    create function update daftar order

--- main/daftarorder.php
+++ main/daftarorder.php
@@ -7,13 +7,30 @@ if(!isset($_SESSION['log'])){
 } else {
 	
 };
+	$uid = $_SESSION['id'];
+	$caricart = mysqli_query($conn,"select * from cart where userid='$uid' and status='Cart'");
+	$fetc = mysqli_fetch_array($caricart);
+	$orderidd = $fetc['orderid'];
+	$itungtrans = mysqli_query($conn,"select count(orderid) as jumlahtrans from cart where userid='$uid' and status!='Cart'");
+	$itungtrans2 = mysqli_fetch_assoc($itungtrans);
+	$itungtrans3 = $itungtrans2['jumlahtrans'];
 	
-	
+	if(isset($_POST["update"])){
+		$kode = $_POST['idproduknya'];
+		$jumlah = $_POST['jumlah'];
+		$q1 = mysqli_query($conn, "update detailorder set qty='$jumlah' where idproduk='$kode' and orderid='$orderidd'");
+		if($q1){
+			echo "Berhasil Update Cart
+			<meta http-equiv='refresh' content='1; url= cart.php'/>";
+		} else {
+			echo "Gagal update cart
+			<meta http-equiv='refresh' content='1; url= cart.php'/>";
+		}
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-<title>Tokopekita - Daftar Belanja</title>
+<title>Selikur Thrift - Daftar Belanja</title>
 <!-- for-mobile-apps -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -188,6 +205,72 @@ if(!isset($_SESSION['log'])){
 						</tr>
 					</thead>
 					
+					<?php 
+					
+						$brg=mysqli_query($conn,"SELECT DISTINCT(idcart), c.orderid, tglorder, status from cart c, detailorder d where c.userid='$uid' and d.orderid=c.orderid and status!='Cart' order by tglorder DESC");
+						$no=1;
+						while($b=mysqli_fetch_array($brg)){
+
+					?>
+					<tr class="rem1"><form method="post">
+						<td class="invert"><?php echo $no++ ?></td>
+						<td class="invert"><a href="order.php?id=<?php echo $b['orderid'] ?>"><?php echo $b['orderid'] ?></a></td>
+						
+						<td class="invert"><?php echo $b['tglorder'] ?></td>
+						<td class="invert">
+						
+						Rp<?php 				$ongkir = 10000;
+												$ordid = $b['orderid'];
+												$result1 = mysqli_query($conn,"SELECT SUM(qty*hargaafter)+$ongkir AS count FROM detailorder d, produk p where d.orderid='$ordid' and p.idproduk=d.idproduk order by d.idproduk ASC");
+												$cekrow = mysqli_num_rows($result1);
+												$row1 = mysqli_fetch_assoc($result1);
+												$count = $row1['count'];
+												if($cekrow > 0){
+													echo number_format($count);
+													} else {
+														echo 'No data';
+													}?>
+						
+						</td>
+				
+						<td class="invert">
+							<div class="rem">
+								<?php
+								if($b['status']=='Payment'){
+								echo '
+								<a href="konfirmasi.php?id='.$b['orderid'].'" class="form-control btn-primary">
+								Konfirmasi Pembayaran
+								</a>
+								';}
+								else if($b['status']=='Diproses'){
+								echo 'Pesanan Diproses (Pembayaran Diterima)';
+								}
+								else if($b['status']=='Dikirim'){
+									echo 'Pesanan Dikirim';
+								} else if($b['status']=='Selesai'){
+									echo 'Pesanan Selesai';
+								} else if($b['status']=='Dibatalkan'){
+									echo 'Pesanan Dibatalkan';
+								} else {
+									echo 'Konfirmasi diterima';
+								}
+								
+								?>
+							</form>
+							</div>
+							<script>$(document).ready(function(c) {
+								$('.close1').on('click', function(c){
+									$('.rem1').fadeOut('slow', function(c){
+										$('.rem1').remove();
+									});
+									});	  
+								});
+						   </script>
+						</td>
+					</tr>
+					<?php
+						}
+					?>
 					
 					
 								<!--quantity-->
