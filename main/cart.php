3877343ddd2afa3268d507a610f63c79e8a266ac
MuliaHartawan <mulia.hartawan011@gmail.com>
2021-05-26 20:30:20 +0700

    add function update cart

--- main/cart.php
+++ main/cart.php
@@ -7,9 +7,26 @@ if (!isset($_SESSION['log'])) {
 } else {
 };
 
+$uid = $_SESSION['id'];
+$caricart = mysqli_query($conn, "select * from cart where userid='$uid' and status='Cart'");
+$fetc = mysqli_fetch_array($caricart);
+$orderidd = $fetc['orderid'];
+$itungtrans = mysqli_query($conn, "select count(detailid) as jumlahtrans from detailorder where orderid='$orderidd'");
+$itungtrans2 = mysqli_fetch_assoc($itungtrans);
+$itungtrans3 = $itungtrans2['jumlahtrans'];
 
-
-
+if (isset($_POST["update"])) {
+    $kode = $_POST['idproduknya'];
+    $jumlah = $_POST['jumlah'];
+    $q1 = mysqli_query($conn, "update detailorder set qty='$jumlah' where idproduk='$kode' and orderid='$orderidd'");
+    if ($q1) {
+        echo "Berhasil Update Cart
+		<meta http-equiv='refresh' content='1; url= cart.php'/>";
+    } else {
+        echo "Gagal update cart
+		<meta http-equiv='refresh' content='1; url= cart.php'/>";
+    }
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -208,6 +225,26 @@ if (!isset($_SESSION['log'])) {
 
                     ?>
                         <tr class="rem1">
+                            <form method="post">
+                                <td class="invert"><?php echo $no++ ?></td>
+                                <td class="invert"><a href="product.php?idproduk=<?php echo $b['idproduk'] ?>"><img src="<?php echo $b['gambar'] ?>" width="100px" height="100px" /></a></td>
+                                <td class="invert"><?php echo $b['namaproduk'] ?></td>
+                                <td class="invert">
+                                    <div class="quantity">
+                                        <div class="quantity-select">
+                                            <input type="number" name="jumlah" class="form-control" height="100px" value="<?php echo $b['qty'] ?>" \>
+                                        </div>
+                                    </div>
+                                </td>
+
+                                <td class="invert">Rp<?php echo number_format($b['hargaafter']) ?></td>
+                                <td class="invert">
+                                    <div class="rem">
+
+                                        <input type="submit" name="update" class="form-control" value="Update" \>
+                                        <input type="hidden" name="idproduknya" value="<?php echo $b['idproduk'] ?>" \>
+
+                            </form>
             </div>
             <script>
                 $(document).ready(function(c) {
