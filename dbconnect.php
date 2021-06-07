f0da90691256020de5cb53495e8c03176f3b3fb1
MuliaHartawan <mulia.hartawan011@gmail.com>
2021-06-07 09:07:23 +0700

    ready to deploy heroku

---
+++ dbconnect.php
@@ -0,0 +1,14 @@
+<?php 
+
+// be2988b2ff3efc
+// a702473e
+// us-cdbr-east-04.cleardb.com
+// mysql://:@/heroku_de8fe86cdf97e2b?reconnect=true
+// isi nama host, username mysql, dan password mysql anda
+$conn = mysqli_connect("localhost","root","","thrift");
+
+if(!$conn){
+	echo "gagal konek database mann";
+} else {
+	
+};
