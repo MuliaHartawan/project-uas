73f2aa987c443d7db808d367db7338258506559c
MuliaHartawan <mulia.hartawan011@gmail.com>
2021-06-07 09:16:37 +0700

    setup dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -3,9 +3,9 @@
 //Get Heroku ClearDB connection information
 
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host1"];
-$cleardb_username = $cleardb_url["user1"];
-$cleardb_password = $cleardb_url["pass1"];
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
 $cleardb_db = substr($cleardb_url["path"], 1);
 $active_group = 'default';
 $query_builder = TRUE;
