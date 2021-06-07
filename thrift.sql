d4c4662b4ab16d4d5db7f885924b2ffe369088c5
MuliaHartawan <mulia.hartawan011@gmail.com>
2021-06-07 09:30:55 +0700

    add file sql

---
+++ thrift.sql
@@ -0,0 +1,315 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 07 Jun 2021 pada 04.15
+-- Versi server: 10.4.18-MariaDB
+-- Versi PHP: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `thrift`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `cart`
+--
+
+CREATE TABLE `cart` (
+  `idcart` int(11) NOT NULL,
+  `orderid` varchar(100) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `tglorder` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status` varchar(10) NOT NULL DEFAULT 'Cart'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `cart`
+--
+
+INSERT INTO `cart` (`idcart`, `orderid`, `userid`, `tglorder`, `status`) VALUES
+(10, '15wKVT0nej25Y', 2, '2020-03-16 12:10:42', 'Selesai'),
+(11, '15Swf8Ye0Fm.M', 2, '2020-03-16 12:17:34', 'Cart'),
+(12, '15PzF03ejd8W2', 1, '2020-05-13 02:40:48', 'Confirmed'),
+(13, '16zG3Fqgv6Lhw', 3, '2021-06-07 01:12:54', 'Selesai'),
+(14, '16HJhEbU9QXCc', 1, '2021-06-07 02:04:13', 'Cart');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detailorder`
+--
+
+CREATE TABLE `detailorder` (
+  `detailid` int(11) NOT NULL,
+  `orderid` varchar(100) NOT NULL,
+  `idproduk` int(11) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `ekspedisi` enum('JNE Express','J&T Express','SiCepat Express','') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `detailorder`
+--
+
+INSERT INTO `detailorder` (`detailid`, `orderid`, `idproduk`, `qty`, `ekspedisi`) VALUES
+(17, '16HJhEbU9QXCc', 6, 1, 'JNE Express');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `idkategori` int(11) NOT NULL,
+  `namakategori` varchar(20) NOT NULL,
+  `tgldibuat` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `kategori`
+--
+
+INSERT INTO `kategori` (`idkategori`, `namakategori`, `tgldibuat`) VALUES
+(1, 'Celana Panjang', '2021-04-01 07:28:34'),
+(2, 'Kemeja', '2021-04-02 07:34:17'),
+(3, 'Hoodie', '2021-04-03 12:15:40');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `konfirmasi`
+--
+
+CREATE TABLE `konfirmasi` (
+  `idkonfirmasi` int(11) NOT NULL,
+  `orderid` varchar(100) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `payment` varchar(10) NOT NULL,
+  `namarekening` varchar(25) NOT NULL,
+  `tglbayar` date NOT NULL,
+  `tglsubmit` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `konfirmasi`
+--
+
+INSERT INTO `konfirmasi` (`idkonfirmasi`, `orderid`, `userid`, `payment`, `namarekening`, `tglbayar`, `tglsubmit`) VALUES
+(1, '15PzF03ejd8W2', 1, 'Bank BCA', 'Admin', '2020-05-16', '2020-05-13 02:41:51');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `login`
+--
+
+CREATE TABLE `login` (
+  `userid` int(11) NOT NULL,
+  `namalengkap` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(150) NOT NULL,
+  `notelp` varchar(15) NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `tgljoin` timestamp NOT NULL DEFAULT current_timestamp(),
+  `role` varchar(7) NOT NULL DEFAULT 'Member',
+  `lastlogin` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `login`
+--
+
+INSERT INTO `login` (`userid`, `namalengkap`, `email`, `password`, `notelp`, `alamat`, `tgljoin`, `role`, `lastlogin`) VALUES
+(1, 'Admin', 'admin', '$2y$10$GJVGd4ji3QE8ikTBzNyA0uLQhiGd6MirZeSJV1O6nUpjSVp1eaKzS', '01234567890', 'Indonesia', '2020-03-16 11:31:17', 'Admin', NULL),
+(2, 'Guest', 'guest', '$2y$10$xXEMgj5pMT9EE0QAx3QW8uEn155Je.FHH5SuIATxVheOt0Z4rhK6K', '01234567890', 'Indonesia', '2020-03-16 11:30:40', 'Member', NULL),
+(3, 'Mulia Hartawan N', 'mulia.hartawan011@gmail.com', '$2y$10$0Lr/JozPxZuKssDBazqPbOH2YpG4jfn2HHKo5rkCWWn3C4jOAbXXO', '083832330659', 'Jl Raden Wijaya No 30 RT 04 RW 05 Desa Sukodadi Kab Lamongan 62253, Sukodadi', '2021-06-07 01:12:14', 'Member', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pembayaran`
+--
+
+CREATE TABLE `pembayaran` (
+  `no` int(11) NOT NULL,
+  `metode` varchar(25) NOT NULL,
+  `norek` varchar(25) NOT NULL,
+  `logo` text DEFAULT NULL,
+  `an` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `pembayaran`
+--
+
+INSERT INTO `pembayaran` (`no`, `metode`, `norek`, `logo`, `an`) VALUES
+(1, 'Bank BCA', '13131231231', 'public/img/bca.jpg', 'Selikur Thrift'),
+(2, 'Bank Mandiri', '943248844843', 'public/img/mandiri.jpg', 'Selikur Thrift'),
+(3, 'DANA', '0882313132123', 'public/img/dana.png', 'Selikur Thrift');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `produk`
+--
+
+CREATE TABLE `produk` (
+  `idproduk` int(11) NOT NULL,
+  `idkategori` int(11) NOT NULL,
+  `namaproduk` varchar(30) NOT NULL,
+  `gambar` varchar(100) NOT NULL,
+  `deskripsi` varchar(200) NOT NULL,
+  `rate` int(11) NOT NULL,
+  `hargabefore` int(11) NOT NULL,
+  `hargaafter` int(11) NOT NULL,
+  `tgldibuat` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `produk`
+--
+
+INSERT INTO `produk` (`idproduk`, `idkategori`, `namaproduk`, `gambar`, `deskripsi`, `rate`, `hargabefore`, `hargaafter`, `tgldibuat`) VALUES
+(5, 3, 'Hoodie GAP', 'produk/16TrkdPLuPQeY.png', 'Size : M Kondisi : Mulus, siap angkut', 4, 150000, 130000, '2021-06-07 02:01:46'),
+(6, 1, 'Wrangler Pants', 'produk/16TJAdzIomauA.png', 'Size : 32 Kondisi : Mulus, siap angkut', 4, 170000, 150000, '2021-06-07 02:02:45'),
+(7, 2, 'Flannel Pair Shine', 'produk/16P4nLSm1RXXQ.png', 'Size : M Kondisi : Mulus, siap angkut', 4, 120000, 100000, '2021-06-07 02:03:32');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`idcart`),
+  ADD UNIQUE KEY `orderid` (`orderid`),
+  ADD KEY `orderid_2` (`orderid`);
+
+--
+-- Indeks untuk tabel `detailorder`
+--
+ALTER TABLE `detailorder`
+  ADD PRIMARY KEY (`detailid`),
+  ADD KEY `orderid` (`orderid`),
+  ADD KEY `idproduk` (`idproduk`);
+
+--
+-- Indeks untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`idkategori`);
+
+--
+-- Indeks untuk tabel `konfirmasi`
+--
+ALTER TABLE `konfirmasi`
+  ADD PRIMARY KEY (`idkonfirmasi`),
+  ADD KEY `userid` (`userid`);
+
+--
+-- Indeks untuk tabel `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`userid`);
+
+--
+-- Indeks untuk tabel `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD PRIMARY KEY (`no`);
+
+--
+-- Indeks untuk tabel `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`idproduk`),
+  ADD KEY `idkategori` (`idkategori`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `idcart` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT untuk tabel `detailorder`
+--
+ALTER TABLE `detailorder`
+  MODIFY `detailid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `idkategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `konfirmasi`
+--
+ALTER TABLE `konfirmasi`
+  MODIFY `idkonfirmasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `login`
+--
+ALTER TABLE `login`
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  MODIFY `no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `produk`
+--
+ALTER TABLE `produk`
+  MODIFY `idproduk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `detailorder`
+--
+ALTER TABLE `detailorder`
+  ADD CONSTRAINT `idproduk` FOREIGN KEY (`idproduk`) REFERENCES `produk` (`idproduk`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `orderid` FOREIGN KEY (`orderid`) REFERENCES `cart` (`orderid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `konfirmasi`
+--
+ALTER TABLE `konfirmasi`
+  ADD CONSTRAINT `userid` FOREIGN KEY (`userid`) REFERENCES `login` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `produk`
+--
+ALTER TABLE `produk`
+  ADD CONSTRAINT `idkategori` FOREIGN KEY (`idkategori`) REFERENCES `kategori` (`idkategori`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
